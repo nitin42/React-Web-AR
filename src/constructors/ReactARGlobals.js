3115aa70f2fd95118010387e1f0729596672273a
Nitin <tulswani19@gmail.com>
2017-11-18 16:06:20 +0530

    wrap up

--- src/constructors/ReactARGlobals.js
+++
@@ -1,8 +0,0 @@
-/**
- * ARToolKit globals
- */
-export const ReactArToolKitContext = window.THREEx.ArToolkitContext
-
-export const ReactArToolKitMarkerController = window.THREEx.ArMarkerControls
-
-export const ReactArToolKitSource = window.THREEx.ArToolkitSource
