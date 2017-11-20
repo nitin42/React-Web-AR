90610493971a9b5f429957f72ce783f282ae3d1c
Nitin <tulswani19@gmail.com>
2017-11-20 21:35:34 +0530

    removed threejs renderer for AR

--- src/TEMP/constructors/ThreeJSConstructors.js
+++
@@ -1,6 +0,0 @@
-const THREE = require('three')
-
-export const WebGl = THREE.WebGLRenderer
-export const Color = THREE.Color
-export const Scene = THREE.Scene
-export const Camera = THREE.Camera
--- src/TEMP/core/react-ar-marker/ARToolKitMarkerControls.js
+++
@@ -1,8 +0,0 @@
-import { ReactArToolKitMarkerController } from '../../constructors/ReactARGlobals'
-import { setARToolKitMarkerControlParameters } from './ARMarkerControlParameters'
-
-// Event fired when the marker is found on the screen! Can be used in some business logic
-// toolKitMarkerControl.addEventListener('markerFound', () => console.log("Hello World! I found the marker"));
-// Remove the 'renderer' argument
-export const initialiseARToolKitMarkerControlController = (parameters, renderer, arContext, root) =>
-	new ReactArToolKitMarkerController(arContext, root, setARToolKitMarkerControlParameters(parameters))
