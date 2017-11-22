8b43a3998dbb749554780236b453e591323a7b31
Nitin <tulswani19@gmail.com>
2017-11-22 23:16:39 +0530

    added threejs renderer component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import AFrameRenderer from './components/aframe/AFrameRenderer'
-import Marker from './components/aframe/Marker'
+import AFrameRenderer from "./components/aframe/AFrameRenderer";
+import Marker from "./components/aframe/Marker";
+import Initialise from "./components/initialise";
+import ARRenderer from "./components/arRenderer";
 
-export {
-  AFrameRenderer,
-  Marker,
-}
+export { AFrameRenderer, Marker, Initialise, ARRenderer };
